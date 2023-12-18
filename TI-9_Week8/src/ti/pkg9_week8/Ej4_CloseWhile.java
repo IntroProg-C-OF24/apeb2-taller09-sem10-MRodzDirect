1697b9c4144e9ebbdbe1a94bde305ea88a75dcd1
MRodz <mabarca1@utpl.edu.ec>
2023-12-18 15:41:43 -0500

    Final

--- TI-9_Week8/src/ti/pkg9_week8/Ej4_CloseWhile.java
+++ TI-9_Week8/src/ti/pkg9_week8/Ej4_CloseWhile.java
@@ -9,7 +9,11 @@ String[] estudiantes = {"Kimberly", "Hogan", "Teresa", "Luis", "Mark", "Jennifer
 String inicial;
 boolean bandera = true;
 int i;
-    
+
+System.out.println("Usted solo puede escribir nombres ejemplo: Luis, mas no puede escribir iniciales de ninguno de los siguientes estudiantes: ");
+for (i=0 ; i < estudiantes.length ; i++){
+    System.out.println(estudiantes[i]);
+}
 while(bandera){
  System.out.println("Ingrese una letra");
     inicial = sc.nextLine();
@@ -17,6 +21,7 @@ while(bandera){
     // Pre-research : if (!inicial.startsWith("K")&&!inicial.startsWith("H")&&!inicial.startsWith("T")&&!inicial.startsWith("L")&&!inicial.startsWith("M")&&!inicial.startsWith("J")&&!inicial.startsWith("A")){
     String est = estudiantes[i];
     if (inicial.equals(estudiantes[i].substring(0,1))){
+        System.out.println("El bucle mientras se ha roto, no escriba iniciales");
     bandera = false;
     }
     }
--- TI-9_Week8/src/ti/pkg9_week8/Ej5_Bimester3_dot_O.java
+++ TI-9_Week8/src/ti/pkg9_week8/Ej5_Bimester3_dot_O.java
@@ -2,26 +2,20 @@ package ti.pkg9_week8;
 public class Ej5_Bimester3_dot_O {
     public static void main(String[] args) {
         
-        int limEsts = 10;
+        int limEsts = 28, estEnc=0, estDeb=0;
         
         double notasEsts [] = new double [limEsts];
-        double sumaNotas =0, promedioNotas = 0, notaMax, notaMin, notasEstz ;
+        double sumaNotas =0, promedioNotas = 0, notaMax, notaMin ;
       
         
         for (int i = 0; i < notasEsts.length; i++) {
        
             notasEsts[i] = (double) (Math.random ()*(9-0+1)+0);
-        }
-        for (int i = 0; i < notasEsts.length; i++) {
-            System.out.printf("%s %d: %.2f \n", "Estudiante",(i+1) ,notasEsts[i]);
-            // printf : %s = cadena, %f = float ; %.xf = only x decimals , %d = entero
-    }
-        for (int i = 0; i < notasEsts.length; i++) {
-            sumaNotas += notasEsts[i];   
+            sumaNotas += notasEsts[i];
+        // printf : %s = cadena, %f = float ; %.xf = only x decimals , %d = entero
         }
         promedioNotas = sumaNotas / notasEsts.length;
-        System.out.printf("Promedio: %.2f \n",promedioNotas);
-            
+        
         notaMax= notasEsts[0];
         notaMin= notasEsts[0];
         for (int i = 1; i < notasEsts.length; i++) {
@@ -35,20 +29,20 @@ public class Ej5_Bimester3_dot_O {
             else if (notasEsts[i]<notaMin){
                 notaMin= notasEsts[i];
             }
-        }
-        System.out.println("Nota mayor: "+notaMax);
-        System.out.println("Nota menor: "+notaMin);
-        
-        for (int i = 0; i < notasEsts.length; i++) {
             if (notasEsts[i]>promedioNotas){
-                System.out.println("Estudiante por encima del promedio:");
-                System.out.printf("Estudiante %d: con nota %2f \n \n", (i+1), notasEsts[i]);}
-            if (notasEsts[i]<promedioNotas){
-                System.out.println("Estudiante por debajo del promedio:");
+                System.out.println("Estudiante por encima del promedio :");
                 System.out.printf("Estudiante %d: con nota %2f \n \n", (i+1), notasEsts[i]);
-            }
-                
+                estEnc++;
+                }
+            else if (notasEsts[i]<=promedioNotas){
+                System.out.println("Estudiantes por debajo o igual al promedio son:");
+                System.out.printf("Estudiante %d: con nota %2f \n \n", (i+1), notasEsts[i]);
+                estDeb++;
+        }       
         }
+        System.out.printf("El total de estudiantes encima del promedio son: %d y debajo o igual al promedio hay: %d \n",estEnc,estDeb);
+        System.out.printf("El promedio del paralelo C es: %.2f \n",promedioNotas);
+        System.out.printf("El estudiante con mejor nota tiene: %.2f y la peor nota es: %.2f \n", notaMax, notaMin);
     
 }
 }
